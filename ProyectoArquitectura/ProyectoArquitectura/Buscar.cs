729f2a9f39deca5c1f214c8ff9cec11eef944cd8
KristelSalas <61443460+KristelSalas@users.noreply.github.com>
2021-05-17 22:03:45 -0600

    Archivo y forma de buscar

---
+++ ProyectoArquitectura/ProyectoArquitectura/Buscar.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace ProyectoArquitectura
+{
+    class Buscar
+    {
+        public void BuscarMetacritic()
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("https://www.google.com");// abrir el navegador
+
+            var busqueda = driver.FindElement(By.Name("q"));//localiza la caja e texto del navegador por el nombre de etiqueta
+            busqueda.SendKeys("meta score");//simula escribir en la caja de texto
+            busqueda.Submit();//realiza la busqueda
+
+            var primerRe = driver.FindElement(By.ClassName("yuRUbf"));//la clase en donde está
+            var link = primerRe.FindElement(By.TagName("a"));//entrar al tag
+            driver.Navigate().GoToUrl(link.GetAttribute("href"));//usar la direccion url
+
+            var busqueda2 = driver.FindElement(By.Name("search_term"));//localiza la caja e texto del navegador por el nombre de etiqueta
+            busqueda2.SendKeys("halo infinite");//simula escribir en la caja de texto
+            busqueda2.Submit();//realiza la busqueda
+        }
+        
+    }
+}
--- ProyectoArquitectura/ProyectoArquitectura/Program.cs
+++ ProyectoArquitectura/ProyectoArquitectura/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.IO;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
 
 namespace ProyectoArquitectura
 {
@@ -6,7 +9,34 @@ namespace ProyectoArquitectura
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            String line;
+            try
+            {
+                //Pass the file path and file name to the StreamReader constructor
+                StreamReader sr = new StreamReader("Juegos.txt");
+                //Read the first line of text
+                line = sr.ReadLine();
+                //Continue to read until you reach end of file
+                while (line != null)
+                {                   
+                    Console.WriteLine(line); //write the line to console window                  
+                    line = sr.ReadLine(); //Read the next line
+                }
+                //close the file
+                sr.Close();
+                Console.ReadLine();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception: " + e.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Executing finally block.");
+            }
         }
     }
+
+
 }
+
